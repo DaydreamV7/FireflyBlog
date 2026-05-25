ceae437fcdb6f791be219f18539c336070d85811
dreamoon <daydream321@qq.com>
2026-05-25 12:59:15 +0800

    chore(配置): 更新了站名和备案

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,7 +7,7 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "梦月Dreammoon",
+	title: "梦的延续",
 
 	// 站点副标题
 	subtitle: "个人博客",
@@ -79,7 +79,7 @@ export const siteConfig: SiteConfig = {
 			// alt: "🍀",
 		},
 		// 导航栏标题
-		title: "梦月Dreammoon",
+		title: "梦的延续",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
